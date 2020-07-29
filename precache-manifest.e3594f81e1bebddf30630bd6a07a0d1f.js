132b0a60b5fa387eb5b77ac0b1542dc69cd0deb5
Christian Otu <christianotu.d@gmail.com>
2020-07-30 00:09:42 +0100

    Updates

---
+++ precache-manifest.e3594f81e1bebddf30630bd6a07a0d1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547d5b9bcb839a2951d8d865d6a5a60a",
+    "url": "/Task-Manager-To-Do-List/index.html"
+  },
+  {
+    "revision": "b71973925c7e1ecfb770",
+    "url": "/Task-Manager-To-Do-List/static/css/main.572306cf.chunk.css"
+  },
+  {
+    "revision": "3d03a5b5d86041caa9cf",
+    "url": "/Task-Manager-To-Do-List/static/js/2.7c0c9a1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Task-Manager-To-Do-List/static/js/2.7c0c9a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71973925c7e1ecfb770",
+    "url": "/Task-Manager-To-Do-List/static/js/main.c1a4ab79.chunk.js"
+  },
+  {
+    "revision": "58645060f00f74042231",
+    "url": "/Task-Manager-To-Do-List/static/js/runtime-main.3446c65a.js"
+  }
+]);
